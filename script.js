eae52c45698c4c443d680771ed09f0eeb4f8fce0
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-03-24 10:18:32 +0100

    ADD condizione chilometri validi

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ let prezzo_biglietto = (tariffa * km_percorsi);
 
 /* condizioni applicazione sconto */
 
-if (numero_anni < 18) { 
+if (numero_anni >= 0 && numero_anni < 18 && km_percorsi > 0) { 
     let prezzo_biglietto_minorenni = prezzo_biglietto - (prezzo_biglietto *20/100);
     alert ('Il prezzo del tuo biglietto è:' + ' ' + prezzo_biglietto_minorenni.toFixed(2) + '€');
  // console.log(prezzo_biglietto_minorenni.toFixed(2)):
 
-}else if (numero_anni >= 65 ) {
+}else if (numero_anni >= 65 && km_percorsi > 0) {
     let prezzo_biglietto_over = prezzo_biglietto - (prezzo_biglietto *40/100);
     alert ('Il prezzo del biglietto è:' + ' ' + prezzo_biglietto_over.toFixed(2) + '€');
  // console.log (prezzo_biglietto_over.toFixed(2));
 
-}else if (numero_anni >18 && numero_anni <= 65 ) {
+}else if (numero_anni >18 && numero_anni <= 65 && km_percorsi > 0) {
     alert ('Il prezzo del biglietto è:' + ' ' + prezzo_biglietto.toFixed(2) + '€');
  // console.log (prezzo_biglietto.toFixed(2));
 
